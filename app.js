77d67d8d689a4a8440f07b5cf9756727febe7eb6
gw12346 <31837855+gw12346@users.noreply.github.com>
2018-11-11 23:54:35 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const fs = require('fs'),
+	express = require('express'),
+	cookieParser = require('cookie-parser'),
+	bodyParser = require('body-parser'),
+	favicon = require('serve-favicon'),
+	socketSession = require('express-socket.io-session'),
+	passport = require('passport'),
+	mongoose = require('mongoose'),
+	compression = require('compression'),
+	Strategy = require('passport-local').Strategy,
+	Account = require('./models/account'),
+	routesIndex = require('./routes/index'),
+	session = require('express-session')({
+		secret: process.env.SECRETSESSIONKEY,
+		resave: false,
+		saveUninitialized: false
+	});
+
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'pug');
+app.locals.pretty = true;
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(favicon(`${__dirname}/public/favicon.ico`));
+app.use(cookieParser());
+app.use(express.static(`${__dirname}/public`, { maxAge: 86400000 * 28 }));
+app.use(session);
+
+io.use(
+	socketSession(session, {
+		autoSave: true
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new Strategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+mongoose.connect(`mongodb://localhost:${process.env.MONGOPORT}/secret-hitler-app`);
+mongoose.Promise = global.Promise;
+
+routesIndex();
---
+++ version.js
@@ -0,0 +1,5 @@
+module.exports = {
+	number: '0.15.0',
+	color: 'customgames0',
+	date: '10-21-2018'
+};
